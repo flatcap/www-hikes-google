b46d3519071a5bdde485fb1062c68658a2fcf49b
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-04-03 14:27:28 +0100

    lat/long/date/msg/route/wp

--- where.php
+++ where.php
@@ -2,6 +2,66 @@
 
 $filename = "rich.json";
 
+function valid_coords ($lat, $lon)
+{
+	// bounds of UK
+	if (($lat < 49) || ($lat > 59))
+		return false;
+
+	if (($lon < -8) || ($lon > 2))
+		return false;
+
+	return true;
+}
+
+function valid_route ($route)
+{
+	if ($route === false)
+		return false;
+	if (empty ($route))
+		return true;
+
+	$path = "web";
+	$filename = "$path/$route/route.xml";
+
+	return file_exists ($filename);
+}
+
+function valid_date ($date)
+{
+	$unix = strtotime ($date);
+	if ($unix == 0)
+		$unix = strtotime ("now");
+
+	return strftime ("%Y-%m-%d", $unix);
+}
+
+function get_waypoint ($route, $wp, &$lat, &$lon)
+{
+	$map = array();
+	$retval = 0;
+
+	$path = "web";
+	$route = "$path/$route/route.xml";
+	$wp = "WP$wp";
+
+	$map = exec ("grep -w $wp $route");
+
+	$parts = explode ('"', $map);
+
+	$conv_lat = $parts[1] / 23860929;
+	$conv_lon = $parts[3] / 11930465;
+
+	if (!valid_coords ($conv_lat, $conv_lon)) {
+		return false;
+	}
+
+	$lat = $conv_lat;
+	$lon = $conv_lon;
+
+	return true;
+}
+
 function get_url_variable($name)
 {
 	$result = false;
@@ -38,7 +98,7 @@ function put_json ($info)
 {
 	global $filename;
 
-	$str = json_encode ($info);
+	$str = json_encode ($info, JSON_PRETTY_PRINT);
 	$str = "rich_info = $str";
 
 	file_put_contents ($filename, $str);
@@ -47,24 +107,42 @@ function put_json ($info)
 function main()
 {
 	$info = get_json();
-	$info->date = strftime ("%Y-%m-%d");
 
 	$lat = get_url_variable ("lat");
 	$lon = get_url_variable ("lon");
 	$msg = get_url_variable ("msg");
 	$dat = get_url_variable ("date");
+	$wpt = get_url_variable ("wp");
+	$rte = get_url_variable ("route");
 
-	if (!empty ($lat)) { $info->latitude  = $lat; }
-	if (!empty ($lon)) { $info->longitude = $lon; }
-	if (!empty ($msg)) { $info->message   = $msg; }
-	if (!empty ($dat)) { $info->date      = $dat; }
+	if (valid_coords ($lat, $lon)) {
+		$info->latitude  = $lat;
+		$info->longitude = $lon;
+	}
 
-	echo "<pre>";
+	if (valid_route ($rte)) {
+		$info->route = $rte;
+	}
 
-	echo "lat  = {$info->latitude}\n";
-	echo "lon  = {$info->longitude}\n";
-	echo "msg  = {$info->message}\n";
-	echo "date = {$info->date}\n";
+	if ($wpt !== false) {
+		if (!get_waypoint ($info->route, $wpt, $info->latitude, $info->longitude)) {
+			$wpt = "invalid";
+		}
+	}
+
+	$info->date = valid_date ($dat);
+
+	if ($msg !== false) {
+		$info->message = $msg;
+	}
+
+	printf ("<pre>\n");
+	printf ("date  = %s (%s)\n", $info->date,strftime ("%A %-d %B %Y", strtotime ($info->date)));
+	printf ("lat   = %0.6f\n",   $info->latitude);
+	printf ("lon   = %0.6f\n",   $info->longitude);
+	printf ("route = %s\n",      $info->route);
+	printf ("wp    = %s\n",      $wpt);
+	printf ("msg   = %s\n",      $info->message);
 
 	put_json ($info);
 }
