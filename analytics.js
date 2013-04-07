c8cd77c56872b025831becfc57ad2108c942b475
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-04-07 01:20:20 +0100

    wrong id

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-377438-2']);
+_gaq.push(['_setAccount', 'UA-377438-1']);
 _gaq.push(['_trackPageview']);
 (function() {
 	var ga = document.createElement('script');
