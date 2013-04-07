19e3bab2016895a2801f676e430bdaf3368cc10c
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-04-07 03:46:45 +0100

    fix sort for chrome

--- rich.js
+++ rich.js
@@ -1710,7 +1710,12 @@ function make_dropdown (selection)
 
 function route_sort(a,b)
 {
-	return (a.fullname > b.fullname);
+	if (a.fullname > b.fullname)
+		return 1;
+	else if (a.fullname < b.fullname)
+		return -1;
+	else
+		return 0;
 }
 
 function select_dropdown (id)
