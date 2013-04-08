74adc4311ba9a479c5423cf39260d3b6f586dd21
Richard Russon (flatcap) <richard.russon@gmail.com>
2013-04-08 14:22:28 +0100

    comment thoroughly

--- rich.js
+++ rich.js
@@ -1,42 +1,74 @@
-var route_list = null;
-var geo    = null;
-var map    = null;
+/**
+ * Copyright (c) 2013 Richard Russon (flatcap)
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see http://www.gnu.org/licenses/ 
+ */
 
-var opt_one  = true;
-var opt_zoom = true;
-var opt_rich = true;
-var marker_rich = null;
+var route_list  = null;		// Array of all route data
+var geo         = null;		// geoXML3 helper
+var map         = null;		// Google map
+			// Options:
+var opt_one     = true;		// Only show one route at a time
+var opt_zoom    = true;		// Zoom in to the current route
+var opt_rich    = true;		// Show Rich's location
+var marker_rich = null;		// Google map marker showing Rich's location
+			// Show list of:
+var show_comp   = true;		// Completed routes
+var show_inco   = false;	// Incomplete routes
+var show_unst   = false;	// Unstarted routes
+var show_hill   = true;		// Sets of hills
 
-var show_comp = true;
-var show_inco = false;
-var show_unst = false;
-var show_hill = true;
+var show_html = new Array();	// Keep the select HTML to rebuild the dropdown
+	show_html["comp"] = "";		// Completed routes
+	show_html["inco"] = "";		// Incomplete routes
+	show_html["unst"] = "";		// Unstarted routes
+	show_html["hill"] = "";		// Sets of hills
 
-var show_html = new Array();
-	show_html["comp"] = "";
-	show_html["inco"] = "";
-	show_html["unst"] = "";
-	show_html["hill"] = "";
+var kml = new Array();		// Display:
+	kml["hike"]    = true;		// Hikes or Climbed hills
+	kml["ferry"]   = true;		// River crossings
+	kml["todo"]    = true;		// Incomplete route sections
+	kml["variant"] = true;		// Variant routes, or detours
+	kml["route"]   = true;		// Official route
+	kml["camp"]    = false;		// Overnight stops
+	kml["area"]    = true;		// Region of hill set
+	kml["start"]   = true;		// Marker showing start of route
+	kml["finish"]  = false;		// Marker showing end of route
+	kml["extra"]   = false;		// Custom data for route
 
-var kml = new Array();
-	kml["hike"]    = true;
-	kml["ferry"]   = true;
-	kml["todo"]    = true;
-	kml["variant"] = true;
-	kml["route"]   = true;
-	kml["camp"]    = false;
-	kml["area"]    = true;
-	kml["start"]   = true;
-	kml["finish"]  = false;
-	kml["extra"]   = false;
+var url_base = "web/";			// Directory containing kml data
 
-var url_base = "web/";
-
+/**
+ * String.contains - Does the string contain this character
+ * @key:	Character to look for
+ *
+ * Extend the String class with a "contains" method.
+ * Search the string for a given character.
+ *
+ * Return: bool
+ */
 String.prototype.contains = function (key)
 {
 	return (this.indexOf (key) >= 0);
 }
 
+/**
+ * initialise - Start up
+ *
+ * Set up the Google map, dropdown and geoXML3 helper.
+ * Set checkboxes to their defaults.
+ */
 function initialise()
 {
 	route_list = init_routes();
@@ -59,6 +91,14 @@ function initialise()
 }
 
 
+/**
+ * make_dropdown - Populate the dropdown box
+ *
+ * Dropdown contents depend on four bools (show_*).
+ * After the rebuild, try to keep the previous selected item.
+ *
+ * Return: DOM select object
+ */
 function make_dropdown()
 {
 	var dd = document.getElementById ("dropdown");
@@ -78,6 +118,12 @@ function make_dropdown()
 	return dd;
 }
 
+/**
+ * select_dropdown - Pick a dropdown entry by value
+ * @route:	Shortname of the route
+ *
+ * Select entry in dropdown by route name
+ */
 function select_dropdown (route)
 {
 	var dd = document.getElementById ("dropdown");
@@ -89,6 +135,13 @@ function select_dropdown (route)
 	}
 }
 
+/**
+ * init_dropdown - Create HTML for dropdown
+ *
+ * Create the HTML for the dropdown from the route_list.
+ *
+ * Return: DOM select object
+ */
 function init_dropdown()
 {
 	var c = new Array();
@@ -161,6 +214,18 @@ function init_dropdown()
 }
 
 
+/**
+ * route_sort - Sort two route_list items by fullname
+ * @a:	Item 1
+ * @b	Item 2
+ *
+ * Sort helper function.
+ * Sort the route_list by fullname.
+ *
+ * Return:	-1	a precedes b
+ * 		 0	a identical to b
+ * 		 1	a follows b
+ */
 function route_sort(a,b)
 {
 	if (a.fullname > b.fullname) {
@@ -172,6 +237,11 @@ function route_sort(a,b)
 	}
 }
 
+/**
+ * init_options - Set the default checkbox values
+ *
+ * Set some sensible defaults for the checkboxes.
+ */
 function init_options()
 {
 	var t;
@@ -200,6 +270,13 @@ function init_options()
 }
 
 
+/**
+ * find_geoxml - Find the geoXML3 document by its url
+ * @url:	url of kml document
+ *
+ * Return:	 n	Index in docs array
+ * 		-1	url doesn't exist
+ */
 function find_geoxml (url)
 {
 	var i;
@@ -213,6 +290,12 @@ function find_geoxml (url)
 	return -1;
 }
 
+/**
+ * hide_other_routes - Hide all routes but this one
+ * @route: Route name
+ *
+ * Hide all the components of all the routes except this one.
+ */
 function hide_other_routes (route)
 {
 	var save_url = get_url (route);
@@ -231,6 +314,12 @@ function hide_other_routes (route)
 	}
 }
 
+/**
+ * hide_route - Hide this route
+ * @route: Route name
+ *
+ * Hide all the components of this route.
+ */
 function hide_route (route)
 {
 	var hide_url = get_url (route);
@@ -249,6 +338,18 @@ function hide_route (route)
 	}
 }
 
+/**
+ * show_rich - Place a marker where Rich was last seen
+ *
+ * Using the data in rich.json, place a marker on the map.
+ *
+ * rich.json contains:
+ *	date:		Date (yyyy-mm-dd), last seen
+ *	latitude:	Degrees latitude (decimal)
+ *	longitude:	Degrees longitude (decimal)
+ *	message:	Message (can contain html)
+ *	route:		Current route
+ */
 function show_rich()
 {
 	// rich_info is from rich.json
@@ -279,6 +380,12 @@ function show_rich()
 }
 
 
+/**
+ * map_create_rich - Create a Google.maps.Marker
+ *
+ * Create a marker to show Rich's location.
+ * Data comes from rich.json (see show_rich() for details).
+ */
 function map_create_rich()
 {
 	// rich_info is from rich.json
@@ -296,6 +403,12 @@ function map_create_rich()
 	});
 }
 
+/**
+ * map_init - Create a Google map
+ *
+ * Create and style a Google map.
+ * When the user presses <Esc>, close the map's infoWindow
+ */
 function map_init()
 {
 	var my_styles = [
@@ -315,7 +428,7 @@ function map_init()
 		streetViewControl: false,
 		overviewMapControl: false,
 		styles: my_styles,
-		mapTypeId: google.maps.MapTypeId.SATELLITE		// ROADMAP, TERRAIN, HYBRID, SATELLITE
+		mapTypeId: google.maps.MapTypeId.SATELLITE	// ROADMAP, TERRAIN, HYBRID, SATELLITE
 	});
 
 	// Escape will close the infoWindow
@@ -329,6 +442,15 @@ function map_init()
 	return map;
 }
 
+/**
+ * map_zoom_ll - Zoom in on coordinates
+ * @lat: Latitude
+ * @lon: Longitude
+ * @zoom: Zoom level (1 = From space, 17 = grass level)
+ *
+ * Centre the map on the coordinates: (@lat, @lon).
+ * Zoom in to the level @zoom.
+ */
 function map_zoom_ll (lat, lon, zoom)
 {
 	if (!lat || !lon || !zoom) {
@@ -350,6 +472,16 @@ function map_zoom_ll (lat, lon, zoom)
 	return true;
 }
 
+/**
+ * map_zoom_route - Frame a route in the map
+ * @route: Route name
+ *
+ * Centre the map on @route and zoom in.
+ * The data come from:
+ *	route_list[@route].latitude
+ *	route_list[@route].longitude
+ *	route_list[@route].zoom
+ */
 function map_zoom_route (route)
 {
 	var lat;
@@ -370,14 +502,27 @@ function map_zoom_route (route)
 }
 
 
+/**
+ * on_change_hike - Event handler for hike dropdown
+ * @id: ID of droprown
+ *
+ * When the user selects a different hike, display it.
+ */
 function on_change_hike (id)
 {
 	var sel = document.getElementById (id);
 	var option = sel.options[sel.selectedIndex].value;
 
-	show_route_new (option);
+	show_route (option);
 }
 
+/**
+ * on_change_kml - Event handler for kml display options
+ * @id: ID of checkbox
+ *
+ * When the user changes a kml display option update the routes
+ * shown on the map accordinly.
+ */
 function on_change_kml (id)
 {
 	var route_key = document.getElementById("dropdown").value;
@@ -387,9 +532,16 @@ function on_change_kml (id)
 
 	kml[name] = check.checked;
 
-	show_route_new (route_key);
+	show_route (route_key);
 }
 
+/**
+ * on_change_opt - Event handler for global options
+ * @id: ID of checkbox
+ *
+ * When the user changes any of the global map options, update
+ * the display of the route(s) accordingly.
+ */
 function on_change_opt (id)
 {
 	var current_route = document.getElementById("dropdown").value;
@@ -412,6 +564,13 @@ function on_change_opt (id)
 }
 
 
+/**
+ * update_route_visibility - Show/hide routes to match dropdown
+ * @id: Route name
+ *
+ * If the user turns off the list of "Complete Routes", then any
+ * routes in that list should be hidden.
+ */
 function update_route_visibility (id)
 {
 	// Hide any routes that aren't displayed in the dropdown
@@ -432,6 +591,17 @@ function update_route_visibility (id)
 	}
 }
 
+/**
+ * on_change_show - Event hander for route list display options
+ * @id: ID of checkbox
+ *
+ * Toggle whether a class of routes is displayed in the dropdown.
+ * The use can choose any, or none of:
+ *	Completed routes
+ *	Incomplete routes
+ *	Unstarted routes
+ *	Sets of hills
+ */
 function on_change_show (id)
 {
 	switch (id) {
@@ -450,6 +620,12 @@ function on_change_show (id)
 	}
 }
 
+/**
+ * on_click_global - Event hander for buttons
+ * @id: ID of button
+ *
+ * Perform some global actions on the map.
+ */
 function on_click_global (id)
 {
 	switch (id) {
@@ -489,7 +665,19 @@ function on_click_global (id)
 }
 
 
-function show_route_new (route)
+/**
+ * show_route - Display/hide a route on the map
+ * @route: Route name
+ *
+ * For a given route, display its data on the map.
+ * Take into account the global options kml[*].
+ *
+ * route_list[@route].attr tells us what kml exists.
+ * kml[*] tells us what the use wants displayed.
+ *
+ * Use show_kml(), hide_kml() to do the work.
+ */
+function show_route (route)
 {
 	if (!(route in route_list)) {
 		return;
@@ -597,6 +785,15 @@ function show_route_new (route)
 	}
 }
 
+/**
+ * get_url - Get the location of the kml for a route
+ * @route: Route name
+ * @type: Data type, e.g. "hike"
+ *
+ * Create a url from the base directory and the route's name and type.
+ *
+ * Return: url of route
+ */
 function get_url (route, type)
 {
 	var url = url_base + route;
@@ -608,6 +805,13 @@ function get_url (route, type)
 	return url;
 }
 
+/**
+ * show_kml - Display one kml on the map
+ * @route: Route name
+ * @type: Data type, e.g. "hike"
+ *
+ * Load and display one kml file on the map.
+ */
 function show_kml (route, type)
 {
 	var url = get_url (route, type);
@@ -619,6 +823,13 @@ function show_kml (route, type)
 	}
 }
 
+/**
+ * hide_kml - Hide one piece of route data
+ * @route: Route name
+ * @type: Data type, e.g. "hike"
+ *
+ * Hide one element of one route on the map.
+ */
 function hide_kml (route, type)
 {
 	var url = get_url (route, type);
